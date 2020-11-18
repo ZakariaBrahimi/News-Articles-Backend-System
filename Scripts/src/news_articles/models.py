67182f124590dc550640c4d15a4f7abb2a185c92
zakaria abdessamed brahimi <brahimi.zakaria.abdessamed@gmail.com>
2020-11-18 09:01:22 +0100

    adding UML Diagram pic

--- Scripts/src/news_articles/models.py
+++ Scripts/src/news_articles/models.py
@@ -6,25 +6,17 @@ class NewsArticle(models.Model):
         ('draft' , 'Draft'),
         ('published', 'Published')
     )
-
-    """
-    you can add as many as categories we want 
-    """
-
-    cayegory_options = (
-        ('politics', 'Politics'),
-        ('health', 'Health'),
-        ('education', 'Education'),
-    )
-    
     summary          = models.TextField()
     content          = models.TextField()
     title            = models.CharField(max_length=300)
     published_date   = models.DateTimeField(auto_now_add=True)
-    category         = models.CharField(max_length=15, choices=cayegory_options)
+    category         = models.ForeignKey(to=ArticleCategory, on_delete=models.CASCADE)
     author           = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='author')
     published_status = models.CharField(max_length=15, choices=published_status_options, default='draft')
     bookmark         = models.ManyToManyField(to=User, default=None, blank=True, related_name='bookmark')
 
     def __str__(self):
         return self.title
+
+class ArticleCategory(models.Model):
+    category_name = models.CharField(max_length=50)
